c50b7e9a1f7ea2dbdb65e1ee870b8273a4bc4058
Raisha58 <2058raisha@gmail.com>
2024-04-29 19:23:46 +0545

    completed score

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ function userFlash(btn) {
 
 function levelUp() {
   userSeq = [];
+  level++;
   h2.innerText = `Level ${level}`;
 
   //random btn choose
@@ -57,8 +58,12 @@ function checkAns(idx) {
       setTimeout(levelUp, 1000);
     }
   } else {
-    h2.innerHTML = `Game Over! Your score was <b>${level}</b> <br><br>
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b> </br></br>
     Press any key to start.`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 150);
     reset();
   }
 }
